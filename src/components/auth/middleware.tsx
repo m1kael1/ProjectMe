8c93566ae5df175e6c15ca71c8069c4542fd264a
mikael <mikaelagungdjawa@gmail.com>
2024-07-05 19:25:14 +0700

    fix: get user login from localstorage

--- src/components/auth/middleware.tsx
+++ src/components/auth/middleware.tsx
@@ -1,40 +1,38 @@
 "use client"
 
 import { useProject } from '@/hooks/use-project'
-import { usePbAuth } from '@/providers/auth-provider'
 import { useParams, useRouter } from 'next/navigation'
 import { PropsWithChildren, useEffect } from 'react'
 
-export const AuthMiddleware = (props: PropsWithChildren) => {
-  const currentUser = usePbAuth().user
+export const AuthMiddleware = ({ children }: PropsWithChildren) => {
+  const currentUser = JSON.parse(localStorage.getItem("pocketbase_auth") as string)
   const router = useRouter()
 
   useEffect(() => {
-    if (!currentUser) {
+    if (!currentUser.model) {
       router.push("/signin")
     }
-  }, [currentUser])
+  })
 
   return (
-    props.children
+    children
   )
 }
 
-export const ProjectMiddleware = (props: PropsWithChildren) => {
-  const currentUser = usePbAuth().user
+export const ProjectMiddleware = ({ children }: PropsWithChildren) => {
+  const currentUser = JSON.parse(localStorage.getItem("pocketbase_auth") as string)
   const { id } = useParams()
   const { projectContributors } = useProject({ projectId: id as string })
   const router = useRouter()
 
-  if (projectContributors.length > 0) {
-    const isContributor = projectContributors.filter((contributor) => contributor.id === currentUser?.id)[0]
-    console.log(isContributor)
+  if (projectContributors.length > 0 && currentUser) {
+    const isContributor = projectContributors.filter((contributor) => contributor.id === currentUser.model.id)[0]
+
     if (!isContributor) {
       router.push("/404")
     }
   }
-
   return (
-    props.children
+    children
   )
 }
