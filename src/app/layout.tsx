cc2a8cbb7613cf2df8c6e7aaeda2b851f13046c1
mikael <mikaelagungdjawa@gmail.com>
2024-07-05 18:00:46 +0700

    disable metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,34 +4,34 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import AuthWrapper from "@/providers/auth-provider";
 
-export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.APP_URL
-      ? `${process.env.APP_URL}`
-      : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : `http://localhost:${process.env.PORT || 3000}`
-  ),
-  title: "shadcn/ui sidebar",
-  description:
-    "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
-  alternates: {
-    canonical: "/"
-  },
-  openGraph: {
-    url: "/",
-    title: "project-management",
-    description:
-      "A easy and powerful project management tool built with Next.js, React, and shadcn/ui.",
-    type: "website"
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "project-management",
-    description:
-      "A easy and powerful project management tool built with Next.js, React, and shadcn/ui."
-  }
-};
+// export const metadata: Metadata = {
+//   metadataBase: new URL(
+//     process.env.APP_URL
+//       ? `${process.env.APP_URL}`
+//       : process.env.VERCEL_URL
+//         ? `https://${process.env.VERCEL_URL}`
+//         : `http://localhost:${process.env.PORT || 3000}`
+//   ),
+//   title: "shadcn/ui sidebar",
+//   description:
+//     "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
+//   alternates: {
+//     canonical: "/"
+//   },
+//   openGraph: {
+//     url: "/",
+//     title: "project-management",
+//     description:
+//       "A easy and powerful project management tool built with Next.js, React, and shadcn/ui.",
+//     type: "website"
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "project-management",
+//     description:
+//       "A easy and powerful project management tool built with Next.js, React, and shadcn/ui."
+//   }
+// };
 
 export default function RootLayout({
   children
